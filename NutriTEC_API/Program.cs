9470eed0e586027cd206cb07b755385a47bdd248
DanielMontoyaR <danimo.rivera@gmail.com>
2023-05-30 23:40:46 -0600

    Ajustes en el program.cs

--- NutriTEC_API/Program.cs
+++ NutriTEC_API/Program.cs
@@ -1,9 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using NutriTEC_API.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<NutriTecPsqlContext>(options =>
+options.UseNpgsql(builder.Configuration.GetConnectionString("NutriTecPsqlContext")));
+
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
