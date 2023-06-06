df6a0f9b513a9238e1e7e5f35be4561b10b9b9b3
DanielMontoyaR <danimo.rivera@gmail.com>
2023-06-05 21:11:01 -0600

    Implementación de logins

--- NutriTEC_API/Controllers/AdminController.cs
+++ NutriTEC_API/Controllers/AdminController.cs
@@ -1,6 +1,36 @@
-﻿namespace NutriTEC_API.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NutriTEC_API.Models;
+
+namespace NutriTEC_API.Controllers
 {
-    public class AdminController
+    [ApiController]
+    [Route("api/")]
+    public class AdminController: ControllerBase
     {
+        private readonly Proyecto2nutritecContext _context;
+
+        public AdminController(Proyecto2nutritecContext context)
+        {
+            _context = context;
+        }
+        [HttpPost("auth_admin")]
+        public async Task<ActionResult<JSON_Object>> AuthAdmin(Credentials Admin_Credentials)
+        {
+            JSON_Object json = new JSON_Object("error", null); //Se inicializa con error y null para ver si hay algun error.
+            var result = _context.LoginAdmins.FromSqlInterpolated($"select * from login_admin({Admin_Credentials.email},{Admin_Credentials.password})");
+            var PGSQL_result = result.ToList();
+            if (PGSQL_result.Count == 0)
+            {
+                return BadRequest(json);
+            }
+            else
+            {
+                json.status = "ok";
+                json.result = PGSQL_result[0];
+                return Ok(json);
+            }
+
+        }
     }
 }
---
+++ NutriTEC_API/Models/LoginAdmin.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NutriTEC_API.Models
+{
+    [Keyless]
+    public class LoginAdmin
+    {
+        public string email { get; set; } = string.Empty;
+    }
+}
