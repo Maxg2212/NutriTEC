bd00c5aa7f08b1d2ccc6a7225e5c6497000b16ac
DanielMontoyaR <danimo.rivera@gmail.com>
2023-06-06 21:13:57 -0600

    Ajustes en los encriptadores

--- NutriTEC_API/Controllers/NutritionistController.cs
+++ NutriTEC_API/Controllers/NutritionistController.cs
@@ -17,6 +17,7 @@ namespace NutriTEC_API.Controllers
         [HttpPost("auth_nutritionist")]
         public async Task<ActionResult<JSON_Object>> AuthNutritionist(Credentials Nutritionist_Credentials)
         {
+            Nutritionist_Credentials.password = MD5Encrypt.EncryptPassword(Nutritionist_Credentials.password);
             JSON_Object json = new JSON_Object("error", null); //Se inicializa con error y null para ver si hay algun error.
             var result = _context.LoginNutritionists.FromSqlInterpolated($"select * from login_nutritionist({Nutritionist_Credentials.email},{Nutritionist_Credentials.password})");
             var PGSQL_result = result.ToList();
@@ -35,6 +36,8 @@ namespace NutriTEC_API.Controllers
         [HttpPost("add_nutritionist")]
         public async Task<ActionResult<JSON_Object>> AddNutritionist(NutritionistData Nutritionist_Data)
         {
+
+            Nutritionist_Data.password = MD5Encrypt.EncryptPassword(Nutritionist_Data.password);
             JSON_Object json = new JSON_Object("error", null); //Se inicializa con error y null para ver si hay algun error.
             var result = _context.InsertNutritionist.FromSqlInterpolated($"select * from insert_nutritionist({Nutritionist_Data.employee_id},{Nutritionist_Data.email},{Nutritionist_Data.name},{Nutritionist_Data.second_name},{Nutritionist_Data.lname1},{Nutritionist_Data.lname2},{Nutritionist_Data.password},{DateOnly.ParseExact(Nutritionist_Data.bdate, "yyyy-MM-dd", CultureInfo.InvariantCulture)},{Nutritionist_Data.profile_pic},{Nutritionist_Data.credit_card},{Nutritionist_Data.nutritionist_code},{Nutritionist_Data.bmi},{Nutritionist_Data.weight},{Nutritionist_Data.address},{Nutritionist_Data.payment_type})");
             var PGSQL_result = result.ToList();
